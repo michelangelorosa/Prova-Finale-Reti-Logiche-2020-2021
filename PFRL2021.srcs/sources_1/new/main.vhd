b2b2b654cb6bca8b8ff0ba863d734bfec1ff89f1
michelangelorosa <79945508+michelangelorosa@users.noreply.github.com>
2021-09-07 10:45:47 +0200

    Project setup

---
+++ PFRL2021.srcs/sources_1/new/main.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+use IEEE.numeric_std.all;
+
+entity project_reti_logiche is
+port (
+i_clk : in std_logic;
+i_rst : in std_logic;
+i_start : in std_logic;
+i_data : in std_logic_vector(7 downto 0);
+o_address : out std_logic_vector(15 downto 0);
+o_done : out std_logic;
+o_en : out std_logic;
+o_we : out std_logic;
+o_data : out std_logic_vector (7 downto 0)
+);
+end project_reti_logiche;
